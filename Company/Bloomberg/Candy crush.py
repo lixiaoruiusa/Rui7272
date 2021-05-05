be753f6798913c0a1b4326279ecb9865ae23dfed
RUI <lirui871124@gmail.com>
2021-05-05 12:30:07 -0400

    +1d candy crash

--- Company/Bloomberg/Candy crush.py
+++ Company/Bloomberg/Candy crush.py
@@ -1,7 +1,5 @@
 
 s = 'abcdecbbbccddd'
-
-
 def remove_triple(s):
     finished = True
     i = len(s) - 1
@@ -17,3 +15,45 @@ def remove_triple(s):
         return remove_triple(s)
 a = remove_triple(s)
 print(a)
+
+
+
+
+def candy_crush(input):
+    if not input:
+        return input
+
+    stack = []
+    stack.append([input[0], 1])
+
+
+    for i in range(1, len(input)):
+        print(stack)
+        if input[i] != input[i - 1]:
+            if stack[-1][1] >= 3:
+                stack.pop()
+            if stack and stack[-1][0] == input[i]:
+                stack[-1][1] += 1
+            else:
+                stack.append([input[i], 1])
+        else:
+            stack[-1][1] += 1
+
+    # handle end
+    if stack[-1][1] >= 3:
+        stack.pop()
+
+    out = []
+    for ltrs in stack:
+        out += ltrs[0] * ltrs[1]
+
+    return ''.join(out)
+
+
+candy_crush("aaaabbbc")
+
+# print(candy_crush("aaaabbbc"))  # c
+# print(candy_crush("aabbbacd"))  # cd
+# print(candy_crush("aabbccddeeedcba"))  # blank expected
+# print(candy_crush("aabbbaacd"))  # cd
+# print(candy_crush("dddabbbbaccccaax"))  # x
