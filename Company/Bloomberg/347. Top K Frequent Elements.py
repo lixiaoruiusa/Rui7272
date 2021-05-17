c50c09fa5e97a4df7967f4a7e78bb34ab8e81815
RUI <lirui871124@gmail.com>
2021-05-17 12:39:27 -0400

    347

---
+++ Company/Bloomberg/347. Top K Frequent Elements.py
@@ -0,0 +1,58 @@
+"""Given an integer array nums and an integer k,
+return the k most frequent elements.
+You may return the answer in any order.
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Input: nums = [1], k = 1
+Output: [1]
+"""
+# O(nlogn) Time | O(n) Space
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        counts = {}
+
+        for i in range(len(nums)):
+            if nums[i] in counts:
+                counts[nums[i]] += 1
+            else:
+                counts[nums[i]] = 1
+
+        heap = []
+
+        for item in counts.items():
+            heapq.heappush(heap, (-item[1], item[0]))
+
+        res = []
+
+        for i in range(k):
+            count, value = heapq.heappop(heap)
+            res.append(value)
+
+        return res
+
+# import heapq
+# nums = [1,1,1,2,2,3]
+# k = 2
+# counts = {}
+#
+# for i in range(len(nums)):
+#     if nums[i] in counts:
+#         counts[nums[i]] += 1
+#     else:
+#         counts[nums[i]] = 1
+#
+# heap = []
+# print(counts)
+# for item in counts.items():
+#     heapq.heappush(heap,(-item[1], item[0]))
+# print(heap)
+#
+# results = []
+#
+# for i in range(k):
+#     count, value = heapq.heappop(heap)
+#     results.append(value)
+#
+# print(results)
