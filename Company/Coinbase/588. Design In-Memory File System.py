403a0a4cc4ce33610a0480a40521275f5f67c5eb
RUI <lirui871124@gmail.com>
2022-05-10 16:29:00 -0400

    btc

--- Company/Coinbase/588. Design In-Memory File System.py
+++ Company/Coinbase/588. Design In-Memory File System.py
@@ -1,5 +1,6 @@
 """
-ls 的时间复杂度是 O(m+n+klog(k))。这里 mm 是输入字符串的长度，我们需要扫描输入字符串一次并获得每一层的文件名。n 是最后一层文件夹的深度，我们需要进入 n 层的文件树以到达最后一层文件所在路径。kk是最后一层的文件和文件夹总数目。我们需要将它们排序，所以需要 klog(k)klog(k) 的时间。
+ls 的时间复杂度是 O(m+n+klog(k))。这里 m 是输入字符串的长度，我们需要扫描输入字符串一次并获得每一层的文件名。
+n 是最后一层文件夹的深度，我们需要进入 n 层的文件树以到达最后一层文件所在路径。k是最后一层的文件和文件夹总数目。我们需要将它们排序，所以需要 klog(k) 的时间。
 
 mkdir 时间复杂度是 O(m+n)，这里 m 是输入字符串的长度，n是最后一层文件夹的深度。
 
@@ -12,7 +13,7 @@ Pro：
 
 从一个路径移动子文件夹到另一个路径也很容易，因为我们需要做的只是获得相应子文件夹类的地址，然后在新文件路径下赋新的值。
 
-如果文件夹数目非常大，我们会因为 isfileisfile 和 contentcontent 浪费多余的空间，这部分空间在方法 1 中不会存在。
+如果文件夹数目非常大，我们会因为 isfile 和 content浪费多余的空间，这部分空间在方法 1 中不会存在。
 
 这个方法的一个问题是如果我们只想要给定路径中文件夹的列表，而不是文件的列表，访问将会变得低效。我们需要遍历当前文件夹所有内容一遍并检查是否是文件夹，才能得到我们想要的数据。
 
@@ -50,7 +51,7 @@ class FileSystem:
                 return []
             # 如果在就继续往下找
             cur = cur.child[ch]
-        # ？
+
         for key in cur.child.keys():
             res.append(key)
         if cur.is_file:
@@ -85,9 +86,99 @@ class FileSystem:
             cur = cur.child[ch]
         return cur.content
 
+"""
+
 # Your FileSystem object will be instantiated and called as such:
 # obj = FileSystem()
 # param_1 = obj.ls(path)
 # obj.mkdir(path)
 # obj.addContentToFile(filePath,content)
 # param_4 = obj.readContentFromFile(filePath)
+
+"""
+
+file_sys = FileSystem() 
+path_listed = file_sys.ls(path)
+file_sys.mkdir(path)
+file_sys.addContentToFile(filePath,content)
+content_filePath = file_sys.readContentFromFile(filePath)
+"""
+
+"""
+    try:
+        cur.execute(
+            query,
+            (row['agent_id'], row['agent_pid'], row['agent_json'],row['agent_json'])
+        )
+        conn.commit()
+        return True, "Records created successfully with agent_id of " + row['agent_id']
+
+    except Exception as e:
+        cur.execute("ROLLBACK")
+        conn.commit()
+        print(e)
+        return False, e
+
+
+# 题目是实现一个in-memory file system, support
+# createFolder(path:string)
+# deleteFolder(path:string)
+# traverseRoot() lists all folders and subfolders from root folder.
+# create和delete需要验证路径是否有效，所有操作都是文件夹，没有文件。
+# follow up 1： traverse‍‍‌‍‌‌‍‍‌‍‌‍‌‌‍‍‍‌‌‍ 输出的结果要按照字典序
+# follow up 2： 现实中实现这个类似的file system，需要考虑那些scalability和reliability的问题
+
+
+# class Trie:
+#
+#     def __init__(self):
+#         self.child = {}
+#         self.name = ""
+#
+# class FileSystem:
+#
+#     def __init__(self):
+#         self.root = Trie()
+#
+#     def is_valid(self, path):
+#         if not path:
+#             return False
+#         if path[0] != "/":
+#             return False
+#         path_list = path[1:].split('/')
+#         for ch in path_list:
+#             if not ch.isalpha():  # isalnum()
+#                 return False
+#         return True
+#
+#     def createFolder(self, path):
+#         if not self.is_valid(path):
+#             return "not valid path"
+#
+#         cur = self.root
+#         path_list = path[1:].split('/')
+#         for ch in path_list:
+#             if ch not in cur.child.keys():
+#                 cur.child[ch] = Trie()
+#             cur = cur.child[ch]
+#             cur.name = ch
+#
+#     def deleteFolder(self, path):
+#         if not self.is_valid(path):
+#             return "not valid path"
+#         cur = self.root
+#         path_list = path[1:].split('/')
+#         for ch in path_list:
+#             if ch not in cur.child.keys():
+#                 cur.child[ch] = Trie()
+#             cur = cur.child[ch]
+#             cur.name = ch
+#
+#     def traverseRoot(self):
+#         cur = self.root
+#         res = []
+#         # 如果path为ls根目录下内容
+#         if path == '/':
+#             for key in cur.child.keys():
+#                 res.append(key)
+#             return sorted(res)
---
+++ Company/Coinbase/BTC.py
@@ -0,0 +1,112 @@
+"""
+一堆交易BTC的记录
+
+output：
+Sell(date: 3, date_acquire=1, amount= 5, proceeds=30.0, gain=6.0)
+Sell(date: 3, date_acquire=2, amount= 2, proceeds=12.0, gain‍‍‌‍‌‌‍‍‌‍‌‍‌‌‍‍‍‌‌‍=2.0)
+……
+打印完全部的sell，如果还有剩余，就把余额打出来
+类似 Buy(date: 6, date_acquire=N, amount= N, price=N)
+注意在处理第三天的卖的时候，虽然把第一天买的BTC卖完了，但是第二天的还有剩余，需要更新第二天的记录，下次卖的时候再处理
+一看是新题有点懵，中间算proceeds和gain把自己弄晕了，最后到时间了，还有bug，肯定挂了
+
+"""
+
+"""
+date: 3, date_acquire=2, amount=2, proceeds=12.0, gain‍‍‌‍‌‌‍‍‌‍‌‍‌‌‍‍‍‌‌‍=2.0
+"""
+import heapq
+# class Btc(object):
+#     def __init__(self) -> None:
+#         self.heap = []
+#         self.date_acquire = None
+#         self.amount = None
+#         self.proceeds = None
+#         self.gain = None
+#
+#     def buy(self, price):
+#         pass
+#     def sell(self, price):
+#         pass
+
+import heapq
+def check_btc(data):
+    res = []
+    heap = []
+    date_acquire = None
+    amount = None
+    proceeds = None
+    gain = None
+
+    for item in input:
+        if item['type'] == "sell":
+
+            if not heap:
+                return "No stock to sell"
+
+            numbers_to_sell = item['amount']
+            while numbers_to_sell > 0 and heap:
+                cur_unit_price = item['price'] / item['amount']
+                unit_price, shares, date = heapq.pop():
+
+                date_acquire = date
+                amount = min(shares, numbers_to_sell)
+
+                # gain = 5 * （42 / 7 - 25 / 5）
+                gain ‍‍‌‍‌‌‍‍‌‍‌‍‌‌‍‍‍‌‌‍= (cur_unit_price - unit_price) * amount
+                proceeds += gain
+
+                cur_res = [date_acquire, amount, proceeds, gain]
+                res.append(cur_res)
+
+                numbers_to_sell -= item['amount']
+                if numbers_to_sell <= 0:
+                    heapq.heappush(heap, [unit_price, shares - numbers_to_sell, date])
+
+
+
+
+            res.append()
+        elif item['type'] == "buy":
+            # {'date': 1, 'amount': 5, 'price': 25.0, 'type': 'buy'}
+            unit_price = item['price'] / item['amount']
+            shares = item['amount']
+            date = item['date']
+            # [单价，数量, 日期]
+            heapq.heappush(heap,[unit_price, shares, date])
+
+
+
+
+
+
+
+
+
+input = [{"date": 1, "amount": 5, "price": 25.0, "type": "buy"},
+{"date": 2,
+"amount" : 20,
+"price": 60.0,
+"type": "buy"
+},
+{"date": 3,
+"amount" : 7,
+"price": 42.0,
+"type": "sell"
+},
+{"date": 4,
+"amount" : 6,
+"price": 30.0,
+"type": "sell"
+},
+{"date": 5,
+"amount" : 5,
+"price": 25.0,
+"type": "buy"
+},
+{"date": 6,
+"amount" : 5,
+"price": 25.0,
+"type": "buy"
+}]
+
