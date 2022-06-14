fe0264a46960d0d61c88687944ebe0a55257aaa6
RUI <lirui871124@gmail.com>
2022-06-13 20:03:13 -0400

    cmp 看看

--- 1.MyLeetcode_tag/排序类（Sort）/27. Remove Element.py
+++ 1.MyLeetcode_tag/排序类（Sort）/27. Remove Element.py
@@ -1,5 +1,20 @@
+
+# 二刷：同向双指针，从0开始：right不是target时就swap，因为相当于默认right是target时就skip
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != val:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+        return left
+
+
 # 题意：把target都move到最后，返回有多少个除了target的元素
 # 思路：其实就是同向双指针，if nums[i] != target，yingxie或者交换，类似move zero
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
 
@@ -25,15 +40,7 @@ class Solution:
         return left
 
 
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
 
-        left = 0
-        for right in range(len(nums)):
-            if nums[right] != val:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-        return left
 
 
 
